eb177163bf88996df3bc800536dbae8d51784b67
Dmitri Sanzharov <dmitrisanzharov@gmail.com>
2023-02-08 19:37:34 +0000

    done

--- src/pages/InvestorRegistration.tsx
+++ src/pages/InvestorRegistration.tsx
@@ -1,71 +1,87 @@
 import React, { useState } from "react";
-
-// view
-import { registrationPageView } from "../view/inputFieldsView";
-
-// components
-import InputField from "../components/InputField";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type Props = {};
 
+type Inputs = {
+	firstName: string;
+	lastName: string;
+	email: string;
+	countryCode: string;
+	telephone: number;
+	password: string;
+	passwordConfirmation: string;
+};
+
 const InvestorRegistration = (props: Props) => {
-	// STATES
-	const [firstNameState, setFirstNameState] = useState<string>("");
-	const [lastNameState, setLastNameState] = useState<string>("");
-	const [emailState, setEmailState] = useState<string>("");
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>();
 
-	// FUNCTIONS
-	function chooseState(arg: string): [string, Function] {
-		if (arg === "firstName") {
-			return [firstNameState, setFirstNameState];
-		}
-
-		if (arg === "lastName") {
-			return [lastNameState, setLastNameState];
-		}
-
-		if (arg === "email") {
-			return [emailState, setEmailState];
-		}
-
-		return [
-			"PLEASE CHECK ARRAY, SOMETHING IS MISSING",
-			(dummyArg) => alert(dummyArg),
-		];
-	}
-
-	// useEffects
-
-	// HTML TEMPLATE
+	const onSubmit: SubmitHandler<Inputs> = (data) => {
+		console.log("submitted data", data);
+	};
 
 	return (
 		<div>
 			<h3>Join today and start investing in minutes.</h3>
 			<hr />
-			{registrationPageView?.map((el) => {
-				const {
-					id,
-					labelText,
-					placeholderText,
-					inputType,
-					warningText,
-					inputNameAndHtmlFor,
-				} = el;
-				return (
-					<div key={id}>
-						<br />
-						<InputField
-							labelTextProp={labelText}
-							placeholderTextProp={placeholderText}
-							inputTypeProp={inputType}
-							warningTextProp={warningText}
-							inputNameAndHtmlForProp={inputNameAndHtmlFor}
-							inputState={chooseState(inputNameAndHtmlFor)[0]}
-							inputUseState={chooseState(inputNameAndHtmlFor)[1]}
-						/>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div>First Name</div>
+				<input
+					{...register("firstName", { required: true })}
+					placeholder="Enter first name"
+				/>
+				{errors?.firstName?.type === "required" && (
+					<div className="warningText">This field is required</div>
+				)}
+				<hr />
+				<div>Last Name</div>
+				<input
+					{...register("lastName", { required: true })}
+					placeholder="Enter last name"
+				/>
+				{errors?.lastName?.type === "required" && (
+					<div className="warningText">This field is required</div>
+				)}
+				<hr />
+				<div>Email</div>
+				<input
+					{...register("email", { required: true, pattern: /.@\w*\../g })}
+					placeholder="Enter email"
+				/>
+				{errors?.email?.type === "required" && (
+					<div className="warningText">This field is required</div>
+				)}
+				{errors?.email?.type === "pattern" && (
+					<div className="warningText">
+						please enter valid email address, example: abc@abc.com
 					</div>
-				);
-			})}
+				)}
+				<hr />
+				<div>Country Code</div>
+				<select
+					{...register("countryCode", { required: true, pattern: /\+\d*/g })}
+				>
+					<option value="select country">Select country</option>
+					<option value="ireland">+353</option>
+					<option value="uk">+44</option>
+					<option value="usa">+1</option>
+					<option value="estonia">+372</option>
+				</select>
+				{errors?.countryCode?.type === "required" && (
+					<div className="warningText">This field is required</div>
+				)}
+				{errors?.countryCode?.type === "pattern" && (
+					<div className="warningText">please select country code</div>
+				)}
+				<hr />
+
+				<input type="submit" />
+			</form>
 		</div>
 	);
 };
